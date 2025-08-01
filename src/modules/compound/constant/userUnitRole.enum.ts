1533a54c6ebdf378d1fe3a88abeb183013943d76
Mahmoud-Khaled-FS <mahmoudkhaled1215@gmail.com>
2025-08-01 22:27:46 +0300

    [DEV] : user unit relation enum

---
+++ src/modules/compound/constant/userUnitRole.enum.ts
@@ -0,0 +1,6 @@
+export enum UserUnitRole {
+  OWNER = 'owner',
+  SUB_OWNER = 'sub_owner',
+  SECURITY = 'security',
+  COMPOUND_OWNER = 'compound_owner',
+}
--- src/modules/compound/entities/unitUser.entity.ts
+++ src/modules/compound/entities/unitUser.entity.ts
@@ -1,6 +1,8 @@
-import { Entity, ManyToOne, Property, type Rel } from '@mikro-orm/core';
+import { Entity, ManyToOne, Property, t, type Rel } from '@mikro-orm/core';
 import { Unit } from './unit.entity';
 import { User } from '../../user/entities/user.entity';
+import { Status } from '../../../core/constant/status.enum';
+import { UserUnitRole } from '../constant/userUnitRole.enum';
 
 @Entity()
 export class UnitUser {
@@ -10,6 +12,9 @@ export class UnitUser {
   @ManyToOne(() => User)
   user!: Rel<User>;
 
-  @Property()
-  role: string = 'owner';
+  @Property({ default: UserUnitRole.OWNER, type: t.string })
+  role: UserUnitRole = UserUnitRole.OWNER;
+
+  @Property({ default: Status.SUSPENDED, type: t.smallint })
+  status: Status = Status.SUSPENDED;
 }
