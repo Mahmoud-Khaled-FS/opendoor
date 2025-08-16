6ed389e25f5931f1183a082caf8ec8497f7e06d5
Mahmoud-Khaled-FS <mahmoudkhaled1215@gmail.com>
2025-08-16 21:48:32 +0300

    fix bug unit id

--- src/modules/compound/responses/unit.response.ts
+++ src/modules/compound/responses/unit.response.ts
@@ -2,11 +2,12 @@ import type { Unit } from '../entities/unit.entity';
 import { compoundResponse } from './compound.response';
 
 export function unitWithCompoundResponse(unit: Unit, user?: any) {
-  const pivot = unit.unitUsers.getItems().find(uu => uu.user.id === user?.id) ?? null;
+  const pivot = unit.unitUsers.getItems().find(uu => uu.user.id === user?.id && uu.unit.id === unit.id) ?? null;
   return {
     id: unit.id,
     name: unit.name,
     role: pivot?.role,
+    status: pivot?.status,
     compound: compoundResponse(unit.compound),
   };
 }
